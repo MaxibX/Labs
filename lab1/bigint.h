1d374d065ac907dd5901d0753b21eee28da22834
MaxibX <114149183+MaxibX@users.noreply.github.com>
2022-10-30 16:39:28 +0700

    started

---
+++ lab1/bigint.h
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<limits.h>
+using namespace std;
+
+class BigInt {
+public:
+    BigInt();
+    BigInt(int);
+    BigInt(string);
+    BigInt(const BigInt&);
+    ~BigInt();
+
+    BigInt& operator=(const BigInt&);
+
+    BigInt operator~() const;
+
+    BigInt& operator++();
+    const BigInt operator++(int);
+    BigInt& operator--();
+    const BigInt operator--(int);
+
+    BigInt& operator+=(const BigInt&);
+    BigInt& operator*=(const BigInt&);
+    BigInt& operator-=(const BigInt&);
+    BigInt& operator/=(const BigInt&);
+    BigInt& operator^=(const BigInt&);
+    BigInt& operator%=(const BigInt&);
+    BigInt& operator&=(const BigInt&);
+    BigInt& operator|=(const BigInt&);
+
+    BigInt operator+() const;
+    BigInt operator-() const;
+
+    bool operator==(const BigInt&) const;
+    bool operator!=(const BigInt&) const;
+    bool operator<(const BigInt&) const;
+    bool operator>(const BigInt&) const;
+    bool operator<=(const BigInt&) const;
+    bool operator>=(const BigInt&) const;
+
+    operator int() const;
+    operator string() const;
+
+    size_t size() const;
+
+    size_t index;
+    bool sign;
+    unsigned int *data;
+};
